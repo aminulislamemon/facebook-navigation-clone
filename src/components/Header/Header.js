336501fea774d4f6b7402ca97a31b71a5da9474d
aminulislamemone <aminulislamemon0005@gmail.comm>
2022-09-28 10:50:21 +0600

    header design complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,13 @@ import React from "react";
 import "./Header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  faBars,
+  faBell,
   faClapperboard,
   faHome,
+  faMessage,
   faSearch,
+  faSortDown,
   faStore,
   faUserGroup,
   faUsersLine,
@@ -13,7 +17,7 @@ import {
 
 const Header = () => {
   return (
-    <div className="w-full h-[70px] shadow-lg flex items-center relative">
+    <div className="w-full h-[70px] shadow-md flex items-center relative bg-white">
       <div className="flex items-center">
         <img className="w-[6.5%]" src={facebook} alt="facebook logo" />
         <div className=" px-6 py-[11px] rounded-full search-field">
@@ -25,29 +29,64 @@ const Header = () => {
           />
         </div>
       </div>
-      <div className="flex text-2xl absolute left-[400px] text-gray-600">
+      <div className="flex text-2xl absolute left-[330px] text-gray-600">
         <FontAwesomeIcon
-          className="px-[30px] py-[25px] ml-[20px] hover:rounded-lg hover:text-blue-500 hover:bg-gray-300 cursor-pointer"
+          className="px-[30px] py-[25px] ml-[20px] hover:rounded-lg hover:text-blue-500 hover:bg-gray-200 cursor-pointer"
           icon={faHome}
         />
         <FontAwesomeIcon
-          className="px-[30px] py-[25px] ml-[20px] hover:rounded-lg hover:text-blue-500 hover:bg-gray-300 cursor-pointer"
+          className="px-[30px] py-[25px] ml-[20px] hover:rounded-lg hover:text-blue-500 hover:bg-gray-200 cursor-pointer"
           icon={faUserGroup}
         />
         <FontAwesomeIcon
-          className="px-[30px] py-[25px] ml-[20px] hover:rounded-lg hover:text-blue-500 hover:bg-gray-300 cursor-pointer"
+          className="px-[30px] py-[25px] ml-[20px] hover:rounded-lg hover:text-blue-500 hover:bg-gray-200 cursor-pointer"
           icon={faClapperboard}
         />
         <FontAwesomeIcon
-          className="px-[30px] py-[25px] ml-[20px] hover:rounded-lg hover:text-blue-500 hover:border2 cursor-pointer"
+          className="px-[30px] py-[25px] ml-[20px] hover:rounded-lg hover:text-blue-500 hover:bg-gray-200 cursor-pointer"
           icon={faStore}
         />
         <FontAwesomeIcon
-          className="px-[30px] py-[25px] ml-[20px] hover:rounded-lg hover:text-blue-500 hover:bg-gray-300 cursor-pointer"
+          className="px-[30px] py-[25px] ml-[20px] hover:rounded-lg hover:text-blue-500 hover:bg-gray-200 cursor-pointer"
           icon={faUsersLine}
         />
       </div>
-
+      <div className="mr-1 flex items-center">
+        <div className="flex items-center  hover:bg-gray-200 pr-12 rounded-full">
+          <img
+            className="w-[40px] h-[40px] rounded-full"
+            src={`https://im.indiatimes.in/amp/2019/May/mark_zuckerberg_1556776740.jpg?w=1200&h=900&cc=1`}
+            alt=""
+          />
+          <h1 className=" font-semibold ml-1">Zukar</h1>
+        </div>
+        <div className="flex pl-5">
+          <div className="w-[45px] h-[45px] rounded-full bg-gray-200 text-center relative mr-3">
+            <FontAwesomeIcon
+              className="rounded-full  bg-gray-100 cursor-pointer relative top-3 text-xl"
+              icon={faBars}
+            />
+          </div>
+          <div className="w-[45px] h-[45px] rounded-full bg-gray-200 text-center relative mr-3">
+            <FontAwesomeIcon
+              className="rounded-full  bg-gray-100 cursor-pointer relative top-3 text-xl"
+              icon={faMessage}
+            />
+          </div>
+          <div className="w-[45px] h-[45px] rounded-full bg-gray-200 text-center relative mr-3">
+            <FontAwesomeIcon
+              className="rounded-full  bg-gray-100 cursor-pointer relative top-3 text-xl"
+              icon={faBell}
+            />
+          </div>
+          <div className="w-[45px] h-[45px] rounded-full bg-gray-200 text-center relative mr-3">
+            <FontAwesomeIcon
+              className="rounded-full  bg-gray-100 cursor-pointer relative top-3 text-xl"
+              icon={faSortDown}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='w-full h-[100vh] text-center bg-gray-100 relative'>
+            <h1 className=' text-3xl font-semibold relative top-28'>Its under construction suckers!!!!!!🚀</h1>
+        </div>
+    );
+};
+
+export default Home;
